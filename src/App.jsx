2dde6f3fca466df89c46b6b96391ae137ca779f1
Mike <mixakievchanen@gmail.com>
2025-08-11 10:23:57 +0300

    розбито по компонентам

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,32 @@
 /* eslint-disable jsx-a11y/accessible-emoji */
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import FilterPanel from './components/FilterPanel/FilterPanel';
 import ProductList from './components/ProductList/ProductList';
 
-// import usersFromServer from './api/users';
-// import categoriesFromServer from './api/categories';
-// import productsFromServer from './api/products';
+import usersFromServer from './api/users';
+import categoriesFromServer from './api/categories';
+import productsFromServer from './api/products';
 
-// const products = productsFromServer.map((product) => {
-//   const category = null; // find by product.categoryId
-//   const user = null; // find by category.ownerId
+const products = productsFromServer.map(product => {
+  const category = // find by product.categoryId
+  const user =  // find by category.ownerId
 
-//   return null;
-// });
+  return null;
+});
 
-export const App = () => (
-  <div className="section">
-    <div className="container">
-      <h1 className="title">Product Categories</h1>
-      <FilterPanel />
+export const App = () => {
+  const [searchQuery, setSearchQuery] = useState('');
+  const productsToShow = [...productsFromServer];
 
-      <ProductList />
+  return (
+    <div className="section">
+      <div className="container">
+        <h1 className="title">Product Categories</h1>
+        <FilterPanel />
 
-      <div className="box table-container">
-        <p data-cy="NoMatchingMessage">
-          No products matching selected criteria
-        </p>
+        <ProductList productsToShow={productsToShow} />
       </div>
     </div>
-  </div>
-);
+  );
+};
---
+++ src/components/ProductItem/ProductItem.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+
+const ProductItem = () => {
+  return (
+    <tbody>
+      <tr data-cy="Product">
+        <td className="has-text-weight-bold" data-cy="ProductId">
+          1
+        </td>
+
+        <td data-cy="ProductName">Milk</td>
+        <td data-cy="ProductCategory">🍺 - Drinks</td>
+
+        <td data-cy="ProductUser" className="has-text-link">
+          Max
+        </td>
+      </tr>
+
+      {/* <tr data-cy="Product">
+        <td className="has-text-weight-bold" data-cy="ProductId">
+          2
+        </td>
+
+        <td data-cy="ProductName">Bread</td>
+        <td data-cy="ProductCategory">🍞 - Grocery</td>
+
+        <td data-cy="ProductUser" className="has-text-danger">
+          Anna
+        </td>
+      </tr>
+
+      <tr data-cy="Product">
+        <td className="has-text-weight-bold" data-cy="ProductId">
+          3
+        </td>
+
+        <td data-cy="ProductName">iPhone</td>
+        <td data-cy="ProductCategory">💻 - Electronics</td>
+
+        <td data-cy="ProductUser" className="has-text-link">
+          Roma
+        </td>
+      </tr> */}
+    </tbody>
+  );
+};
+
+export default ProductItem;
